3321e0a909fe411ac27e9058715aaad6ca923f83
Chhun_Natharith <johnathanrith@gmail.com>
2025-05-13 13:57:52 +0700

    PG

--- lib/views/pages/profile_page.dart
+++ lib/views/pages/profile_page.dart
@@ -9,21 +9,79 @@ class ProfilePage extends StatefulWidget {
 
 class _ProfilePageState extends State<ProfilePage> {
   TextEditingController controller = TextEditingController();
+  bool? isChecked = false;
+  bool isSwitch = false;
+  double sliderValue = 0.05;
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(20.0),
-      child: Column(
-        children: [
-          TextField(
-            controller: controller,
-            decoration: InputDecoration(border: OutlineInputBorder()),
-            onEditingComplete: () {
-              setState(() {});
-            },
-          ),
-          Text(controller.text),
-        ],
+    return SingleChildScrollView(
+      child: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Column(
+          children: [
+            TextField(
+              controller: controller,
+              decoration: InputDecoration(border: OutlineInputBorder()),
+              onEditingComplete: () => {setState(() {})},
+            ),
+            Text(controller.text),
+            Checkbox(
+              tristate: true,
+              value: isChecked,
+              onChanged: (bool? value) {
+                setState(() {
+                  isChecked = value;
+                });
+              },
+            ),
+            CheckboxListTile(
+              tristate: true,
+              title: Text("Click me!"),
+              value: isChecked,
+              onChanged: (bool? value) {
+                setState(() {
+                  isChecked = value;
+                });
+              },
+            ),
+            Switch(
+              value: isSwitch,
+              onChanged:
+                  (value) => {
+                    setState(() {
+                      isSwitch = value;
+                    }),
+                  },
+            ),
+            SwitchListTile(
+              title: Text("Click me!"),
+              value: isSwitch,
+              onChanged:
+                  (value) => {
+                    setState(() {
+                      isSwitch = value;
+                    }),
+                  },
+            ),
+            Slider(
+              value: sliderValue,
+              onChanged:
+                  (value) => {
+                    setState(() {
+                      sliderValue = value;
+                    }),
+                  },
+            ),
+            InkWell(
+              onTap: () => {print('Image selected!')},
+              child: Container(
+                height: 50,
+                width: double.infinity,
+                color: Colors.white38,
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
